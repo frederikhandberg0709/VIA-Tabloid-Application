ea20b92309825f5981048527e9908c0dbd74e9cf
FREDERIK DIAMOND <hello@frederikhandberg.com>
2025-06-10 17:17:48 +0200

    Missing /public folder

--- viatab-frontend/next.config.ts
+++ viatab-frontend/next.config.ts
@@ -5,17 +5,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${
-          process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api"
-        }/:path*`,
-      },
-    ];
-  },
-
   env: {
     NEXT_PUBLIC_API_URL:
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api",
